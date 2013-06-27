afc8b2920c48d71fed5e9b85c518dd95aeac5023
Patty <p.moj88@googlemail.com>
2013-06-27 12:09:04 +0200

    Erstes Test-Programm mit bewegendem Schriftzug
    Inkl. Random, Koordinaten, Farben, Zeitverzögerung

---
+++ Tests/Test-Intro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pipikaka = "~#*♥ CaNdYlAnD ♥*#~";
+
+            // Color = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            for (int i = 0; i <= 100; i++)
+            {
+                Random random = new Random();
+                int x = random.Next(0, 50);
+                int y = random.Next(0, 50);
+                Console.SetCursorPosition(x, y);
+                Console.WriteLine(pipikaka);
+                Thread.Sleep(500);
+                Console.Clear();
+            }
+            Console.ReadLine();
+        }
+    }
+}
