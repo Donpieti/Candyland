144824470e1009cd18e123b2e4c5d65e423f5873
Patty <p.moj88@googlemail.com>
2013-06-27 14:52:52 +0200

    Hilfe: Field-Klasse
    (Lädt/schreibt die Map, Koordinaten, schreibt einzelne Felder neu...)

---
+++ Tests/Hilfe zum Grundgerüst/Field.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    public class Field
+    {
+        public char Text;
+        public ConsoleColor Color = ConsoleColor.White;
+    }
+}
