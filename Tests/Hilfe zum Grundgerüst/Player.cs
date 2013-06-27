c1fea7f02769ee5c33355b32cc216e20e25c2423
Patty <p.moj88@googlemail.com>
2013-06-27 14:55:00 +0200

    Hilfe:
    Player-Position wird neu angezeigt, Koordinaten für Player

---
+++ Tests/Hilfe zum Grundgerüst/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    public class Player
+    {
+        public int x;
+        public int y;
+
+        public Player(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Render()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write('X');
+        }
+    }
+}
