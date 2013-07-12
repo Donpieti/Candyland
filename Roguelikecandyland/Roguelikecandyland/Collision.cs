f18083e83a2dd8a7ca4d0a0462fb7ccf36b5e61d
Patty <p.moj88@googlemail.com>
2013-07-12 09:34:57 +0200

    Neue Klasse für Kollisionsabfragen

---
+++ Roguelikecandyland/Roguelikecandyland/Collision.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Roguelikecandyland
+{
+    public class collision
+    {
+        public Boolean wall(int x, int y)
+        {
+            if (GameManager.map.GetField(x + 1, y + 1).Text == 'o')
+            {
+                return true;
+            }
+            else
+            { 
+                return false; 
+            }
+        }
+
+
+        public Boolean door(int x, int y)
+        {
+            if (GameManager.map.GetField(x + 1, y + 1).Text == 'x') //Türsymbol?
+            {
+                return true;
+            }
+            else
+            { 
+                return false; 
+            }
+        }
+    }
+}
